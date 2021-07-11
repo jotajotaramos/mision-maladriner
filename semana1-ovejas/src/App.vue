0625745cca686bbf5c19ac5085da1dfad05448c8
jotajotaramos <jramosj@gmail.com>
2021-07-11 18:19:39 +0200

    Week 1. Sheep counter. Finished

---
+++ semana1-ovejas/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <SheepCounter msg="Welcome to Your Sheep Counter App"/>
+</template>
+
+<script>
+import SheepCounter from './components/SheepCounter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SheepCounter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ semana1-ovejas/src/components/SheepCounter.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>Contador de ovejas</h1>
+    <form @submit.prevent="onSubmit">
+        <input type="text" name="number" v-model="add">
+        <input type="submit" value="Sumar" >
+    </form>  
+    <h3>Acumulador de ovejas: {{numberOfSheeps}}</h3>
+    <h3 v-if="numberOfSheeps == 100" style="color:red;">Ojo! Has llegado a las 100 ovejas</h3>
+</template>
+
+<script>
+export default {
+
+    data(){
+
+        return{
+            numberOfSheeps: 0,
+            add: '',
+        }
+
+    },
+    methods:{
+
+        onSubmit(){
+
+            this.numberOfSheeps+= Number(this.add);
+
+        }
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
